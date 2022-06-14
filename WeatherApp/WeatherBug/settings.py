66e814c5fe605a1e9c4527e2ecd86d9da4589b2a
alex13243 <96782840+Al6ex@users.noreply.github.com>
2022-06-14 18:15:10 +0200

    fixes

--- WeatherApp/WeatherBug/settings.py
+++ WeatherApp/WeatherBug/settings.py
@@ -48,8 +48,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'WeatherBug.urls'
 
 TEMPLATES = [
