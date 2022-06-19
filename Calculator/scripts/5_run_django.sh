30f3e6a29dcc5f13da8729a7e6766ab3c549aad5
alex13243 <96782840+Al6ex@users.noreply.github.com>
2022-06-19 08:30:56 +0200

    New App
    Calculator -  in black (dark) mode

---
+++ Calculator/scripts/5_run_django.sh
@@ -0,0 +1,9 @@
+cd ../
+
+source env/bin/activate
+
+python3 manage.py runserver 127.0.0.1:8000
+
+
+
+sh
---
+++ Calculator/scripts/9_create_superuser.sh
@@ -0,0 +1,9 @@
+cd ../
+
+source env/bin/activate
+
+
+python manage.py createsuperuser --username alex --email ott.alexander.2006@gmail.com
+
+
+sh
